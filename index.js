7f291e9971f42e41c1fd7055e5731b7edd7b7d4b
Praveen K <127839101+thepraveen-k@users.noreply.github.com>
2024-02-14 15:31:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+const products = {
+    men: ["images/shopify1.png", "images/shopify2.png", "images/shopify3.png", "images/shopify4.png"],
+    women: ["images/shopify5.png", "images/shopify6.png", "images/shopify7.png", "images/shopify8.png"],
+    kids: ["images/shopify9.png", "images/shopify10.png", "images/shopify11.png", "images/shopify12.png"]
+};
+
+function filterProducts(category) {
+    
+    document.getElementById('menProducts').style.display = 'none';
+    document.getElementById('womenProducts').style.display = 'none';
+    document.getElementById('kidsProducts').style.display = 'none';
+
+    
+    document.getElementById(category + 'Products').style.display = 'flex';
+
+   
+    const container = document.getElementById(category + 'Products');
+    container.innerHTML = '';
+    products[category].forEach((image, index) => {
+        let discountLabel = '';
+        switch (index % 4) {
+            case 0:
+                discountLabel = 'Wedding Special';
+                break;
+            case 2:
+                discountLabel = 'On Offer';
+                break;
+            case 3:
+                discountLabel = 'New Season';
+                break;
+        }
+
+        const card = `
+            <div class="product-card">
+                <img class="product-image" src="${image}" alt="Product">
+                <div class="discount-label">${discountLabel}</div>
+                <h2 class="product-title">${category} Product</h2>
+                <div class="price-container">
+                    <p class="product-price">Rs 1199.00 </p>
+                    &nbsp;&nbsp;
+                    <strike class="product-original-price"> 1299.00</strike>
+                    <p class="product-price discount">50% Off</p>
+                </div>
+                <button class="add-to-cart-btn">Add to Cart</button>
+            </div>
+        `;
+        container.innerHTML += card;
+    });
+}
